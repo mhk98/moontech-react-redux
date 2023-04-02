02c56b34a7053cd3043e1eb1c4595419706befcb
mhk98 <mohsinkabirseo@gmail.com>
2023-04-02 15:24:41 +0600

    moontech

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import ProductProvider from "./context/ProductProvider";
+import store from "./redux/store";
+import routes from "./routes/routes";
+
+function App() {
+  return (
+    <div>
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+
+      </Provider>
+
+
+    </div>
+  );
+}
+
+export default App;
